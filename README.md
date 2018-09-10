4beadee9d6bf4d68af10429cffe0db18df63a7cd
Lukas Mohrbacher <lukas.mohrbacher@diconium.com>
2018-09-10 10:59:59 +0200

    lets talk bluetooth

--- README.md
+++ README.md
@@ -19,7 +19,14 @@ Let's write an awesome PWA with web bluetooth
 
 ### Web Bluetooth API
 
+chrome://flags/#enable-experimental-web-platform-features
+
 * [inspiration](https://www.youtube.com/watch?v=_4nrh6mTt4E&feature=youtu.be&t=8h19m47s)
 * bluetooth terminal ([app](https://github.com/1oginov/Web-Bluetooth-Terminal), [library](https://github.com/1oginov/bluetooth-terminal))
 * [samples](https://googlechrome.github.io/samples/web-bluetooth/) ([source](https://github.com/GoogleChrome/samples/tree/gh-pages/web-bluetooth))
-* [gatt](http://www.novelbits.io/bluetooth-gatt-services-characteristics/)
+* [gatt](https://www.bluetooth.com/specifications/gatt/services), [defines](https://github.com/adafruit/Adafruit_nRF52_Arduino/blob/master/libraries/Bluefruit52Lib/src/BLEUuid.h)
+  * Services
+    * [Environmental Sensing](https://www.bluetooth.com/specifications/gatt/viewer?attributeXmlFile=org.bluetooth.service.environmental_sensing.xml)  0x181A
+  * Characteristics
+    * [Temperature](https://www.bluetooth.com/specifications/gatt/viewer?attributeXmlFile=.bluetooth.characteristic.temperature.xml) 0x2A1E, 0x2A1F
+    * [Measurement Interval](https://www.bluetooth.com/specifications/gatt/viewer?attributeXmlFile=org.bluetooth.characteristic.measurement_interval.xml) 0x2A21
--- hardware/main/main.ino
+++ hardware/main/main.ino
@@ -10,6 +10,16 @@
 #define B 4608  // material constant B
 #define RV 68000 // series resistor in Ohm
 
+#define UUID16_SVC_ENVIRONMENTAL_SENSING    0x181A
+#define UUID16_CHR_TEMPERATURE_MEASUREMENT0 0x2A1E
+#define UUID16_CHR_TEMPERATURE_MEASUREMENT1 0x2A1F
+
+BLEService        envservice = BLEService(UUID16_SVC_ENVIRONMENTAL_SENSING);
+BLECharacteristic characteristic_temp0 = BLECharacteristic(UUID16_CHR_TEMPERATURE_MEASUREMENT0);
+BLECharacteristic characteristic_temp1 = BLECharacteristic(UUID16_CHR_TEMPERATURE_MEASUREMENT1);
+
+BLEDis bledis; // DIS (Device Information Service) helper class instance
+
 // https://learn.adafruit.com/bluefruit-nrf52-feather-learning-guide/nrf52-adc
 void setupADC() {
   analogReference(AR_VDD4);
@@ -39,6 +49,84 @@ float adcSeriesResistance(int input) {
   return (RV*input/MAXANALOGREAD / (1-input/MAXANALOGREAD)) / 1000;
 }
 
+char tempADC(int input) {
+	char buffer[6];
+  float temp = tempNTCB(analogRead(input)) - ABSZERO;
+	return snprintf(buffer, sizeof buffer, "%f", temp*100);
+}
+
+void connect_callback(uint16_t conn_handle)
+{
+  char central_name[32] = { 0 };
+  Bluefruit.Gap.getPeerName(conn_handle, central_name, sizeof(central_name));
+
+  Serial.print("Connected to ");
+  Serial.println(central_name);
+}
+
+void disconnect_callback(uint16_t conn_handle, uint8_t reason)
+{
+  (void) conn_handle;
+  (void) reason;
+
+  Serial.println("Disconnected");
+  Serial.println("Advertising!");
+}
+
+void setupBLE() {
+	// Initialise the Bluefruit module
+  Serial.println("Initialise the Bluefruit nRF52 module");
+  Bluefruit.begin();
+  Bluefruit.setName("Bluefruit52");
+
+  // Set the connect/disconnect callback handlers
+  Bluefruit.setConnectCallback(connect_callback);
+  Bluefruit.setDisconnectCallback(disconnect_callback);
+
+  // Configure and Start the Device Information Service
+  Serial.println("Configuring the Device Information Service");
+  bledis.setManufacturer("Adafruit Industries");
+  bledis.setModel("Bluefruit Feather52");
+  bledis.begin();
+
+  // BLEService and BLECharacteristic classes
+  Serial.println("Configuring the Service");
+  envservice.begin();
+	characteristic_temp0.setProperties(CHR_PROPS_INDICATE);
+	characteristic_temp0.setPermission(SECMODE_OPEN, SECMODE_NO_ACCESS);
+	characteristic_temp0.begin();
+	characteristic_temp0.write("0");
+
+  // Setup the advertising packet(s)
+  Serial.println("Setting up the advertising payload(s)");
+    // Advertising packet
+  Bluefruit.Advertising.addFlags(BLE_GAP_ADV_FLAGS_LE_ONLY_GENERAL_DISC_MODE);
+  Bluefruit.Advertising.addTxPower();
+
+  // Include HTM Service UUID
+  Bluefruit.Advertising.addService(envservice);
+
+  // Include Name
+  Bluefruit.Advertising.addName();
+
+  /* Start Advertising
+   * - Enable auto advertising if disconnected
+   * - Interval:  fast mode = 20 ms, slow mode = 152.5 ms
+   * - Timeout for fast mode is 30 seconds
+   * - Start(timeout) with timeout = 0 will advertise forever (until connected)
+   *
+   * For recommended advertising interval
+   * https://developer.apple.com/library/content/qa/qa1931/_index.html
+   */
+  Bluefruit.Advertising.restartOnDisconnect(true);
+  Bluefruit.Advertising.setInterval(32, 244);    // in unit of 0.625 ms
+  Bluefruit.Advertising.setFastTimeout(30);      // number of seconds in fast mode
+	Bluefruit.Advertising.start(0); // 0 = Don't stop advertising after n seconds
+
+  Serial.println("Ready Player One!!!");
+	Serial.println("\nAdvertising");
+}
+
 void printADC(char *name, int input) {
   int adcvalue = analogRead(input);
 
@@ -58,17 +146,25 @@ void setup() {
   Serial.begin(BAUDRATE);
 
   setupADC();
+	setupBLE();
 }
 
 void loop() {
-  if (analogRead(A0) < MAXANALOGREAD) {
-    printADC("Tip", A0);
-    Serial.print("  |  ");
-    printADC("Handle", A1);
-    Serial.println();
-  } else {
-    Serial.println("probe not connected");
-  }
+	digitalToggle(LED_RED);
 
-  delay(100);
+  if ( Bluefruit.connected() ) {
+		if (analogRead(A0) == 0) {
+			char temp = tempADC(A0);
+			characteristic_temp0.write(&temp);
+			Serial.print("BLE value updated to ");
+			Serial.print(temp);
+			Serial.println("°C");
+		} else {
+			characteristic_temp0.write("0");
+			Serial.println("probe not connected");
+		}
+	}
+
+  // Only send update once per second
+	delay(1000);
 }
